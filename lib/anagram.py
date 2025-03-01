b1274b38fce042a31fe4078db15bdc5de1315bf7
Happiness Maina <mainahappiness361@gmail.com>
2025-03-01 17:07:52 +0300

    add file

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram():
+    def __init__(self,word):
+        self.word=word.lower()
+        self.sorted_word=sorted(self.word)
+
+    def match (self,words):
+        return [word for word in words if sorted(word.lower()) == self.sorted_word and word.lower() != self.word]
+
+        pass
+        pass
+
